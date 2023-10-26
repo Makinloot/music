3eb9d1a5d422761b9ee3ecf5a57e73eb50d94f99
Tornike <itorn9777@gmail.com>
2023-10-26 16:36:44 +0400

    install neccesary packages for app

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
+import "dotenv/config";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 export { app };
